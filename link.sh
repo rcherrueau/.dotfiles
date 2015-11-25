2067addf4dc67bb5cd3187bf346e7ee3519f815f
RFish ⚓ <RonanCherrueau@gmail.com>
2015-11-26 00:57:24 +0100

    Documentation for nixos

--- link.sh
+++ link.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 # http://brandon.invergo.net/news/2012-05-26-using-gnu-stow-to-manage-your-dotfiles.html
+# On nixos name the `.bash_profile` as `.xprofile`
+# Reason: SLiM login manager doesn't call .bash_profile at login
+# https://github.com/NixOS/nixpkgs/issues/5200
+# https://bbs.archlinux.org/viewtopic.php?id=174916
 
 stow --target="$HOME" system
 stow --target="$HOME" git
