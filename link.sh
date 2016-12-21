437c75ca4204b1ce7bf1fd0c6c89e2e87f177380
RFish ⚓ <RonanCherrueau@gmail.com>
2016-12-21 10:54:25 +0100

    Manage home/hp nix configuration from link.sh

--- link.sh
+++ link.sh
@@ -1,13 +1,27 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
 # http://brandon.invergo.net/news/2012-05-26-using-gnu-stow-to-manage-your-dotfiles.html
 # On nixos name the `.bash_profile` as `.xprofile`
 # Reason: SLiM login manager doesn't call .bash_profile at login
 # https://github.com/NixOS/nixpkgs/issues/5200
 # https://bbs.archlinux.org/viewtopic.php?id=174916
 
-stow --target="$HOME"       system
-stow --target="$HOME"       git
-stow --target="$HOME"       spacemacs
-stow --target="/etc/nixos/" nixos
+# Link `specific.nix` file
+case "$1" in
+  home)
+    ln -v -frs nixos/home-specific.nix nixos/specific.nix
+    ;;
+  hp)
+    ln -v -frs nixos/hp-specific.nix nixos/specific.nix
+    ;;
+  *)
+    echo "Is this home or hp computer? recall with $0 home or $0 hp"
+    exit 1
+    ;;
+esac
 
-# TODO: case home/hp ln -s specific
+stow -v --target="$HOME" system
+stow -v --target="$HOME" git
+stow -v --target="$HOME" spacemacs
+
+sudo stow -v --ignore=".*specific.nix" --target="/etc/nixos" nixos
