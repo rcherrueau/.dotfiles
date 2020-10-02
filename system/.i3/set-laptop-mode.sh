b134ed77b9cf80fe54b1960debf2fa7ec37297b3
RFish ⚓ <RonanCherrueau@gmail.com>
2020-10-02 14:54:02 +0200

    Change name of dell-desktop mode to IMT

--- system/.i3/set-laptop-mode.sh
+++ system/.i3/set-laptop-mode.sh
@@ -3,14 +3,14 @@
 # Let me switch between laptop and desktop mode for my hp using rofi.
 
 case "$1" in
-  dell-desktop)
+  IMT)
       sed -i 's/^Xft\*dpi:/!&/' /home/rfish/.dotfiles/system/.Xresources
       xrdb /home/rfish/.dotfiles/system/.Xresources
       xrandr --output DP1-1 --auto --output eDP1 --off
       feh --bg-fill $HOME/.background-image
       >&2 i3 restart
       sudo rfkill block wifi
-      >&2 echo 'dell-desktop mode activated'
+      >&2 echo 'IMT mode activated'
       ;;
   laptop)
       sed -i 's/^!Xft\*dpi:/Xft*dpi:/' /home/rfish/.dotfiles/system/.Xresources
@@ -22,7 +22,7 @@ case "$1" in
       >&2 echo 'hp laptop mode activated'
       ;;
   *)
-      echo "dell-desktop"
+      echo "IMT"
       echo "laptop"
       ;;
 esac
--- system/.i3/toggle-mode.sh
+++ system/.i3/toggle-mode.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# Toggle between laptop and dell-desktop mode
+# Toggle between laptop and IMT mode
 function toggle-screen() {
     grep -q '^!Xft\*dpi:' /home/rfish/.dotfiles/system/.Xresources
     if [ $? -eq 0 ]; then
         set-laptop-mode.sh 'laptop'
     else
-        set-laptop-mode.sh 'dell-desktop'
+        set-laptop-mode.sh 'IMT'
     fi
 }
