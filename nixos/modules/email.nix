9dcbeb38863148f70598101c00ebb9b5a9a405d8
RFish ⚓ <RonanCherrueau@gmail.com>
2020-10-12 18:43:36 +0200

    [email] Better specification of email accounts
    Save to restate common parameters with inherit

--- nixos/modules/email.nix
+++ nixos/modules/email.nix
@@ -2,12 +2,10 @@
 
 let
   # --------------------------------------------------------------------
-  # Email accounts definition:
-
-  accounts = [
-    { # Gmail
+  # Email accounts:
+  accounts = rec {
+    Gmail = {
       default = true;
-      name = "Gmail";
       email = "RonanCherrueau@gmail.com";
       imap.host = "imap.gmail.com";
       smtp.host = "smtp.gmail.com";
@@ -16,9 +14,8 @@ let
       signature = pkgs.writeText "gmail-sign" ''
         Ronan-Alexandre Cherrueau
       '';
-    }
-    { # Inria
-      name = "Inria";
+    };
+    Inria = {
       email = "Ronan-Alexandre.Cherrueau@inria.fr";
       imap.host = "zimbra.inria.fr";
       smtp.host = "smtp.inria.fr";
@@ -29,21 +26,16 @@ let
         Ronan-A. Cherrueau
         https://rcherrueau.github.io
       '';
-    }
-    { # IMT
+    };
+    IMT = {
       # https://intranet.imt-atlantique.fr/assistance-support/informatique/didacticiels/
-      name = "IMT";
+      inherit (Inria) signature trash;
       email = "Ronan-Alexandre.Cherrueau@imt-atlantique.fr";
       imap.host = "z.imt.fr";
       smtp.host = "z.imt.fr";
       keepass = "/root/IMT/rcherr12";
-      trash = "Trash";
-      signature = pkgs.writeText "inria-sign" ''
-        Ronan-A. Cherrueau
-        https://rcherrueau.github.io
-      '';
-    }
-  ];
+    };
+  };
 
   # --------------------------------------------------------------------
   # Notmuch tagging script
@@ -52,7 +44,7 @@ let
     # > notmuch tag +inria -- path:inria/**
     ${lib.concatMapStrings (name: ''
     +${name} -- path:${name}/**
-    '') (map (lib.getAttr "name") accounts)}
+    '') accountNames}
 
     +ISP -- from:freetelecom.fr or from:free-mobile.fr or from:assistance.free.fr
     +Banque -- from:ca-atlantique-vendee.fr or from:ing.com
@@ -71,45 +63,45 @@ let
     CopyArrivalDate yes  # Keeps the time stamp based message sorting intact
 
     # Declare accounts
-    ${lib.concatMapStringsSep "\n\n" (acc: ''
-      IMAPAccount ${acc.name}
-      User        ${acc.email}
-      Host        ${acc.imap.host}
+    ${lib.concatMapStringsSep "\n" (name: with accounts.${name}; ''
+      IMAPAccount ${name}
+      User        ${email}
+      Host        ${imap.host}
       SSLType     IMAPS
-      PassCmd     "${getPasswd} ${acc.keepass}"
-    '') accounts}
+      PassCmd     "${getPasswd} ${keepass}"
+    '') accountNames}
 
     # A Store defines a collection of mailboxes; basically a folder,
     # either local or remote. Here we specify the remote and local Store:
 
     ## - The remote store (IMAPStore) is where we get the email from
     ##   (e.g., the specification of an imap account)
-    ${lib.concatMapStringsSep "\n\n" (acc: ''
-      IMAPStore ${acc.name}-remote
-      Account   ${acc.name}
-    '') accounts}
+    ${lib.concatMapStringsSep "\n" (name: ''
+      IMAPStore ${name}-remote
+      Account   ${name}
+    '') accountNames}
 
     ## - The local store (MaildirStore) is where we store the email on
     ##   our computer.
-    ${lib.concatMapStringsSep "\n\n" (acc: ''
-      MaildirStore ${acc.name}-local
-      Path         ${mailDir}/${acc.name}/
-      Inbox        ${mailDir}/${acc.name}/inbox
+    ${lib.concatMapStringsSep "\n" (name: ''
+      MaildirStore ${name}-local
+      Path         ${mailDir}/${name}/
+      Inbox        ${mailDir}/${name}/inbox
       SubFolders   Verbatim
-    '') accounts}
+    '') accountNames}
 
     # A Channel connects two Stores, describing the way the two are
     # synchronized.
-    ${lib.concatMapStringsSep "\n\n" (acc: ''
-      Channel  ${acc.name}-inbox
-      Master   :${acc.name}-remote:
-      Slave    :${acc.name}-local:
-      Patterns "*" "![Gmail]*" "${acc.trash}" # Copy all remote email box as is except [Gmail]
+    ${lib.concatMapStringsSep "\n" (name: with accounts.${name}; ''
+      Channel  ${name}-inbox
+      Master   :${name}-remote:
+      Slave    :${name}-local:
+      Patterns "*" "![Gmail]*" "${trash}" # Copy all remote email box as is except [Gmail]
       Sync     All    # Propagate read, deletion ...
       Create   Slave  # Automatically create missing mailboxes on the Slave.
       Expunge  Slave  # Only delete on Slave
       MaxSize  100m   # Don't download any email greater than this
-    '') accounts}
+    '') accountNames}
   '';
 
   # msmtp configuration file (man msmtp)
@@ -121,17 +113,17 @@ let
     syslog          on
 
     # Accounts
-    ${lib.concatMapStringsSep "\n\n" (acc: ''
-      account       ${acc.name}
-      host          ${acc.smtp.host}
+    ${lib.concatMapStringsSep "\n" (name: with accounts.${name}; ''
+      account       ${name}
+      host          ${smtp.host}
       port          587
-      from          ${acc.email}
-      user          ${if acc.smtp ? "user" then acc.smtp.user else acc.email}
-      passwordeval  ${getPasswd} ${acc.keepass}
-    '') accounts}
+      from          ${email}
+      user          ${if smtp ? user then smtp.user else email}
+      passwordeval  ${getPasswd} ${keepass}
+    '') accountNames}
 
     # Set a default account
-    account default : ${(findDefaultAccount accounts).name}
+    account default : ${defaultAccount.name}
   '';
 
   # notmuch configuration file (man notmuch-config)
@@ -141,8 +133,9 @@ let
 
     [user]
     name=Ronan-Alexandre Cherrueau
-    primary_email=${(findDefaultAccount accounts).email}
-    other_email=${lib.concatMapStringsSep ";" (lib.getAttr "email") accounts}
+    primary_email=${defaultAccount.value.email}
+    other_email=${builtins.concatStringsSep ";"
+      (lib.catAttrs "email" (builtins.attrValues accounts))}
 
     # Configuration for "notmuch new"
     # - tags:	A list (separated by ';') of the tags that will be
@@ -171,10 +164,10 @@ let
   # Astroid configuration
   # See https://github.com/astroidmail/astroid/wiki/Configuration-Reference
   astroidConfig = (pkgs.formats.json {}).generate "astroid-config.json" (astroidDefaultConfig // {
-    accounts = lib.foldr (a: b: a // b) {} (map (acc: with acc; { ${name} = {
+    accounts = builtins.mapAttrs (name: acc: with acc; {
       name = "Ronan-Alexandre Cherrueau";
       email = "${email}";
-      sendmail = "${msmtpWp} -i --read-recipents --account=${acc.name}";
+      sendmail = "${msmtpWp}/bin/msmtpq --read-envelope-from --read-recipients --account=${name}";
       always_gpg_sign = false;
       save_sent = true;
       save_sent_to = "${mailDir}/${name}/sent/cur/";
@@ -182,11 +175,11 @@ let
       signature_file = "${signature}";
       signature_separate = true;
       default = if acc ? default then default else false;
-    };}) accounts);
+    }) accounts;
     startup.queries =
       # { Inria = "tag:inbox and tag:Inria"; Gmail = "tag:inbox and tag:Gmail" }
-      lib.genAttrs (map (lib.getAttr "name") accounts) (name: "tag:inbox and tag:${name}");
-    poll.interval = 0; # Disable automatic polling
+      lib.genAttrs accountNames (name: "tag:inbox and tag:${name}");
+    poll.interval = 0; # Disable automatic polling from astroid (managed by systemd)
     astroid = {
       notmuch_config = notmuchConfig;
       debug.dryrun_sending = true;
@@ -194,9 +187,15 @@ let
       log.syslog = true;
     };
     editor = {
-      cmd = "emacs --parent-id %3 --eval '(progn (find-file \"%1\") (setq mail-setup-with-from nil) (mail-mode))'";
+      cmd = ''
+        emacs --parent-id %3 --eval '(progn (find-file "%1")
+                                            (setq mail-setup-with-from nil)
+                                            (mail-mode))'
+      '';
       external_editor = false;
-      attachement_words = lib.concatStringsSep "," [ "attach" "p.-j." "pièce jointe" "pièce-jointe" "ci-joint"];
+      attachement_words = lib.concatStringsSep ","
+        [ "attach" "attachement"
+          "p.-j." "pièce jointe" "pièce-jointe" "ci-joint"];
     };
     # Thread index is the "list of emails" view
     thread_index.cell = {
@@ -212,19 +211,73 @@ let
     mail.send_delay = 20;  # Wait 20 seconds before sending email
   });
 
+  # Move deleted emails to trash box and delete old emails.
+  deleteMails =
+    let getTrashBox = name: with accounts.${name}; "${name}/${trash}";
+        trashBoxNames = map getTrashBox accountNames;
+        trashBoxesQuery = lib.concatMapStringsSep " OR " (box: "folder:${box}") trashBoxNames;
+    in pkgs.writers.writeBash "delete-emails" ''
+      # Move emails of a notmuch QUERY into BOX
+      #
+      # Note: mbsync adds a unique identifier to file names (e.g.,
+      # `/path/to/mail,U=<UID>:2,SR` -- with `2` stands for the version of
+      # UID generation if I am right).  Moving files causes UID conflicts
+      # and prevent mbsync from syncing with "Maildir error: UID 9610 is
+      # beyond highest assigned UID 86."  The sed command in the following
+      # removes the UID to force mbsync to regenerate one and avoid UID
+      # conflicts.
+      function moveToBox {
+        local QUERY="$1"
+        local BOX="$2"
+
+        for EMAIL_PATH in $(${notmuchWp}/bin/notmuch search --output=files "$QUERY")
+        do
+          # Strip UID from email name
+          EMAIL_BASENAME=$(basename "$EMAIL_PATH")
+          EMAIL_NO_UID=$(echo "$EMAIL_BASENAME" | sed -r 's/U=[0-9]+:2/U=:2/g')
+          EMAIL_IN_BOX="${mailDir}/$BOX/cur/$EMAIL_NO_UID"
+          # Move email to $BOX
+          echo "Move email from $EMAIL_PATH to $EMAIL_IN_BOX"
+          mv "$EMAIL_PATH" "$EMAIL_IN_BOX"
+        done
+      }
+
+      # # Delete emails older than 30 days
+      # ${notmuchWp}/bin/notmuch search --output=files --format=text0 \
+      #   tag:deleted and date:..30days \
+      #   | xargs -0 --no-run-if-empty rm
+
+      # Move emails marked as deleted into the Trash box
+      ${lib.concatMapStrings (name: ''
+         moveToBox "tag:${name} AND tag:deleted NOT (${trashBoxesQuery})" \
+                   ${getTrashBox name}
+      '') accountNames}
+
+      # Remove the inbox tag for emails in trash box (this also
+      # systematically tags emails as +deleted in case some emails have
+      # been deleted from the webmail interface)
+      ${notmuchWp}/bin/notmuch tag -inbox +deleted -- ${trashBoxesQuery}
+    '';
+
   # --------------------------------------------------------------------
   # Utils
 
+  # List of account names
+  accountNames = builtins.attrNames accounts;
+
+  # The default account -> {name = defaultAccountName; value = defaultAccountValues;}
+  defaultAccount =
+    let errorMsg = "cannot find a default email account";
+        # Test if an account has the `default` key set to `true`
+        isDefault = acc: acc ? default && acc.default;
+        # List of account values with a special attribute name
+        accountWithNames = lib.mapAttrsToList (name: acc: acc // {name = name;} ) accounts;
+        # Find the first default account
+        acc = lib.lists.findFirst isDefault (abort errorMsg) accountWithNames;
+    in { name = acc.name; value = acc; };
+
   # Directory to store emails
   mailDir = config.users.users.rfish.home + "/.mail";
-
-  # Default account
-  findDefaultAccount = accounts:
-    let errorMsg = "cannot find a default email account";
-    in lib.lists.findFirst
-        # Get the acc.default key or false
-        (acc: if acc ? default then acc.default else false)
-        (abort errorMsg) accounts;
 
   # Read password from KeePassX
   getPasswd = pkgs.writers.writeDash "kpcli-read-passwd" ''
@@ -239,59 +292,6 @@ let
 
     # Display it
     echo "$PASSWD"
-  '';
-
-  # Move deleted emails to trash box and delete old emails.
-  #
-  # Note: mbsync adds a unique identifier to file names (e.g.,
-  # `/path/to/mail,U=<UID>:2,SR` -- with `2` stands for the version of
-  # UID generation if I am right).  Moving files causes UID conflicts
-  # and prevent mbsync from syncing "Maildir error: UID 9610 is beyond
-  # highest assigned UID 86."  The sed command in the following
-  # removes the UID to force mbsync to regenerate one and avoid UID
-  # conflicts.
-  deleteMails =
-    let getTrashBox = acc: lib.escapeShellArg "${acc.name}/${acc.trash}";
-    in pkgs.writers.writeBash "delete-emails" ''
-      # Delete emails older than 30 days
-      ${notmuchWp}/bin/notmuch search --output=files --format=text0 \
-        tag:deleted and date:..30days \
-        | xargs -0 --no-run-if-empty rm
-
-      # Move emails of a notmuch QUERY into BOX
-      function moveToBox {
-        local QUERY="$1"
-        local BOX="$2"
-
-        for EMAIL_PATH in $(${notmuchWp}/bin/notmuch search --output=files "$QUERY")
-        do
-          # Strip UID from email name
-          EMAIL_BASENAME=$(basename "$EMAIL_PATH")
-          EMAIL_NO_UID=$(echo "$EMAIL_BASENAME" | sed -r 's/U=[0-9]+:2/U=:2/g')
-          # Move email to $BOX
-          mv "$EMAIL_PATH" "${mailDir}/$BOX/cur/$EMAIL_NO_UID"
-        done
-      }
-
-      # Move emails marked as deleted into the Trash box
-      ${lib.concatMapStrings (acc: ''
-         moveToBox "tag:${acc.name} AND tag:deleted NOT folder:${getTrashBox acc}" \
-                   ${getTrashBox acc}
-      '') accounts}
-
-      # Remove the inbox tag from deleted mails (this also
-      # systematically tags emails as +deleted in case some emails have
-      # been deleted from the webmail interface)
-      ${notmuchWp}/bin/notmuch tag -inbox +deleted -- ${lib.concatMapStringsSep " OR " (acc: ''
-         folder:${getTrashBox acc}'') accounts}
-    '';
-
-  # Create the directory for local email stores
-  mkLocalStores = pkgs.writers.writeDash "msbync-local-stores" ''
-    ${lib.concatMapStrings (name: ''
-       mkdir -p ${mailDir}/${name}/
-       chown ${config.users.users.rfish.name}:${config.users.users.rfish.group} ${mailDir}/${name}/
-    '') (map (lib.getAttr "name") accounts)}
   '';
 
   # Create the default astroid config
@@ -385,7 +385,7 @@ in {
 
   # Configure mbsync + notmuch
   # See https://wiki.archlinux.org/index.php?title=Isync&oldid=627584#Automatic_synchronization
-  systemd.user.services.mbsync =
+  systemd.user.services.polling-email =
     let HOME = config.users.users.rfish.home;
     in {
       description = "Mailbox synchronization service";
@@ -396,12 +396,12 @@ in {
         ExecStartPre =  [
           # Build the local store if any
           mkLocalStores
-
+          # Notify astroid about new polling
+          #
           # XXX: `astroid --start-polling` ends with a coredump but
           # seems to work!  I prefixed the command with a dash `-` to
           # tell systemd to ignore the result.
           "-${astroidWp}/bin/astroid --start-polling"
-
           # Deleted emails may not work if `notmuch new` has not been
           # executed first.  I prefixed the command with a dash `-` to
           # tell systemd to ignore the result.
