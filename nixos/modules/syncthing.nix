96048f95f44ad6117774efb9ffde16cd4e28ef60
RFish ⚓ <RonanCherrueau@gmail.com>
2021-12-18 23:37:46 +0100

    [nix] Update syncthing configuration

--- nixos/modules/syncthing.nix
+++ nixos/modules/syncthing.nix
@@ -13,17 +13,17 @@ with lib;
       openDefaultPorts = true;
 
       # I have to copy this from my private secret folder
-      declarative.cert = builtins.toPath "/etc/nixos/secret/${device-name}-syncthing-cert.pem";
-      declarative.key = builtins.toPath "/etc/nixos/secret/${device-name}-syncthing-key.pem";
+      cert = builtins.toPath "/etc/nixos/secret/${device-name}-syncthing-cert.pem";
+      key = builtins.toPath "/etc/nixos/secret/${device-name}-syncthing-key.pem";
 
-      declarative.devices = {
+      devices = {
         home =      { id = "6IWZNUJ-S3TO23F-SHT6UGV-MFX3U5O-S5DNPT2-MEEWFVN-ZUPFIPG-YMAKFAI"; };
         hp =        { id = "U3CRJ5I-KFRTLQP-SYMPZ6H-PTEK535-FUMPKEG-KLBFJQI-4KPVSCA-3LLDLAP"; };
         jolla =     { id = "RH4MLPV-RUN3EUH-GDL6YVB-2GAD0VZ-JVYZ4SA-WP25PTB-XHCVKW4-VINKRQR"; };
         donatello = { id = "EPFSPKC-CQAEOMP-F7MNWBV-2N4FGA5-446F4MF-34GJD3B-DXPAPAQ-A6PBEQL"; };
       };
 
-      declarative.folders = {
+      folders = {
         "${config.users.users.rfish.home}/secret" = {
           id = "secret";
           devices = [ "home" "hp" "jolla" "donatello" ];
